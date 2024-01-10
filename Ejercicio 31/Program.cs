5354131bf5e15c495f6a6b2416d301f840bf9e03
EricRequena <eric.requena.7e7@itb.cat>
2024-01-10 13:47:25 +0100

    hfoiwhf

--- Ejercicio 31/Program.cs
+++ Ejercicio 31/Program.cs
@@ -3,33 +3,36 @@ namespace Ejercicio_31
 {
     public class Program
     {
-        public static void Main( )
+        public static void Main()
         {
             const string MsgIntNome = "Selecciona el nom d'usuari";
             const string MsgIntNumber = "Introdueix un nombre dins del rang [1,500]";
+            string name = "Eric";
+            int number = 0;
+            Random rand = new Random();
 
-            Console.WriteLine( MsgIntNome );
-            NameRang();
+            Console.WriteLine(MsgIntNome);
+            Console.WriteLine(NameRang(ref name));
             Console.WriteLine(MsgIntNumber);
-            NumbreRang();
-            Console.WriteLine(GenerateRandomNumber);
+            Console.WriteLine(NumbreRang(ref number));
+            Console.WriteLine(GenerateRandomNumber());
         }
 
-        public static void NameRang( )
+        public static string NameRang(ref string name)
         {
-            string name;
             do
             {
                 name = Console.ReadLine();
             } while (name.Length > 10);
+            return name;
         }
-        public static void NumbreRang( )
+        public static int NumbreRang(ref int number)
         {
-            int number;
             do
             {
                 number = Convert.ToInt32(Console.ReadLine());
             } while (number < 1 || number > 500);
+            return number;
         }
 
         public static int GenerateRandomNumber()
